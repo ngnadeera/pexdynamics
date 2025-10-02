822902064aceaacea3a513f0e89b6453b0da2cc7
nadeera <nadeera.gayantha@paraqum.com>
2025-10-02 11:16:27 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+   output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true,
+  // Your repo is 'pexdynamics' → project page
+  basePath: "/pexdynamics",
+  assetPrefix: "/pexdynamics/",
   
 };
 
